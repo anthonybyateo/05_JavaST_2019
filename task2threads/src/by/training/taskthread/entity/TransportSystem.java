3f32fd68050b18c495e585bfacc5262189defc02
anthonybyateo <anthony.by.ateo@gmail.com>
2019-03-12 19:11:25 +0300

    update task3

--- task2threads/src/by/training/taskthread/entity/TransportSystem.java
+++
@@ -1,7 +0,0 @@
-package by.training.taskthread.entity;
-
-import java.util.concurrent.CopyOnWriteArrayList;
-
-public class TransportSystem {
-    private CopyOnWriteArrayList<Route> stopList;
-}
--- task2threads/src/by/training/taskthread/main/CommonResourse.java
+++
@@ -1,5 +0,0 @@
-package by.training.taskthread.main;
-
-public class CommonResourse {
-    public int x = 0;
-}

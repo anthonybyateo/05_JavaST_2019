6fa084d7e8b110d399197d872f9214d7c2d8002e
anthonybyateo <anthony.by.ateo@gmail.com>
2019-03-30 23:06:12 +0300

    update task4

---
+++ task4/src/by/training/taskxml/parser/saxbuilder/SAXBullder.java
@@ -0,0 +1,15 @@
+package by.training.taskXML.parser;
+
+import by.training.taskXML.entity.tariffs.TariffType;
+
+public class SAXBullder extends BaseBuilder {
+    @Override
+    public TariffType getTariffType() {
+        return null;
+    }
+
+    @Override
+    public void buildTariffType(TariffType tariff) {
+
+    }
+}
